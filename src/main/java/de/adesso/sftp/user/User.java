34b87678b3b2bf43ab6d1e32889a9176e2043978
ivan <ivan.kablar@live.com>
2023-10-02 01:15:09 +0200

    Added Equals and HAshCode methods.

--- src/main/java/de/adesso/sftp/user/User.java
+++ src/main/java/de/adesso/sftp/user/User.java
@@ -3,6 +3,8 @@ package de.adesso.sftp.user;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Objects;
+
 @Configuration
 @ConfigurationProperties(prefix = "user")
 public class User {
@@ -31,5 +33,18 @@ public class User {
                 "pubkey='" + pubkey + '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(pubkey, user.pubkey) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pubkey, name);
     }
 }

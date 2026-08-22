0365c4e5c7aa30802af65d07516bdf65718ced48
Ivan Kablar <52032139+IvanKablar@users.noreply.github.com>
2026-08-23 00:39:46 +0200

    Public-Key-Vergleich vereinfachen

--- src/main/java/de/adesso/sftp/authentication/PublicKeyService.java
+++ src/main/java/de/adesso/sftp/authentication/PublicKeyService.java
@@ -21,80 +21,52 @@ public class PublicKeyService {
     private static final Logger log = LogManager.getLogger(PublicKeyService.class);
 
     /**
-     * Validiert und vergleicht die Public-Keys des Servers und des Clients. Stimmen beide Schlüssel übereinstimmen, ist
-     * der Public-Key des Clients gültig.
+     * Validiert den auf dem Server konfigurierten Public-Key und vergleicht ihn mit dem Public-Key des Clients.
+     * Stimmen beide Schlüssel überein, ist der Public-Key des Clients gültig.
      *
-     * @param serverConfPublicKey der auf dem Server konfigurierte Public-Key.
-     * @param clientSentPublicKey der vom Client beim Anmelden geschickte Public-Key.
+     * @param serverConfPublicKey der auf dem Server konfigurierte Public-Key (authorized_keys-Format).
+     * @param clientPublicKey     der vom Client beim Anmelden geschickte Public-Key.
      * @param serverSession       die Server Session.
-     * @return true, wenn die Public-Key valide und gleich sind, sonst false.
+     * @return true, wenn der konfigurierte Public-Key valide ist und beide Schlüssel gleich sind, sonst false.
      */
-    public boolean isPublicKeyValid(String serverConfPublicKey, String clientSentPublicKey, ServerSession serverSession) {
+    public boolean isPublicKeyValid(String serverConfPublicKey, PublicKey clientPublicKey, ServerSession serverSession) {
         PublicKey serverPublicKey = getServerPublicKey(serverConfPublicKey, serverSession);
-        if (serverPublicKey == null) return false;
-
-        PublicKey clientPublicKey = getClientPublicKey(clientSentPublicKey, serverSession);
-        if (clientPublicKey == null) return false;
+        if (serverPublicKey == null || clientPublicKey == null) {
+            return false;
+        }
         return compareKeys(clientPublicKey, serverPublicKey);
     }
 
     /**
-     * Holt den Public-Key des Clients.
-     * @param clientSentPublicKey ist der Public-Key, den der Benutzer/Client schickt.
-     * @param serverSession ist die ServerSession.
-     * @return den Public-Key, wenn er valide ist oder null, wenn nicht.
-     */
-    private PublicKey getClientPublicKey(String clientSentPublicKey, ServerSession serverSession) {
-        PublicKey clientPublicKey;
-        try {
-            clientPublicKey = generatePublicKey(clientSentPublicKey, serverSession);
-        } catch (IOException e) {
-            log.warn("Fehler beim Dekodieren des clientseitigen Public-Keys", e);
-            return null;
-        } catch (IllegalArgumentException e) {
-            log.warn("Der clientseitige Public-Key besitzt kein gültiges Format", e);
-            return null;
-        } catch (GeneralSecurityException e) {
-            log.warn("Fehler beim Generieren des clientseitigen Public-Keys", e);
-            return null;
-        }
-        return clientPublicKey;
-    }
-
-    /**
-     * Holt den Public-Key des Clients.
+     * Holt den auf dem Server konfigurierten Public-Key.
      * @param serverConfPublicKey ist der Public-Key, den der Server hinterlegt hat.
      * @param serverSession ist die ServerSession.
      * @return den Public-Key, wenn er valide ist oder null, wenn nicht.
      */
     private PublicKey getServerPublicKey(String serverConfPublicKey, ServerSession serverSession) {
-        PublicKey serverPublicKey;
         try {
-            serverPublicKey = generatePublicKey(serverConfPublicKey, serverSession);
+            return parsePublicKey(serverConfPublicKey, serverSession);
         } catch (IOException e) {
             log.warn("Fehler beim Dekodieren des serverseitigen Public-Keys", e);
-            return null;
         } catch (IllegalArgumentException e) {
             log.warn("Der serverseitige Public-Key besitzt kein gültiges Format", e);
-            return null;
         } catch (GeneralSecurityException e) {
             log.warn("Fehler beim Generieren des serverseitigen Public-Keys", e);
-            return null;
         }
-        return serverPublicKey;
+        return null;
     }
 
     /**
      * Analysiert den Public-Key im String und prüft, ob dieser ein gültiges Format besitzt, dekodiert werden kann
-     * und ein Objekt vom Typ <java.security.PublicKey> generiert werden kann.
+     * und ein Objekt vom Typ <code>java.security.PublicKey</code> generiert werden kann.
      *
      * @param publicKey     der zu analysierende Public-Key im String-Format.
      * @param serverSession die Server Session
-     * @return den generiert Public-Key vom typ <code>java.security.PublicKey</code>
+     * @return den generierten Public-Key vom Typ <code>java.security.PublicKey</code>
      * @throws IOException              falls der Public-Key nicht dekodiert werden kann.
      * @throws GeneralSecurityException falls der Rückgabeparameter nicht generiert werden kann.
      */
-    private PublicKey generatePublicKey(String publicKey, ServerSession serverSession) throws IOException, GeneralSecurityException {
+    private PublicKey parsePublicKey(String publicKey, ServerSession serverSession) throws IOException, GeneralSecurityException {
         if (publicKey == null || publicKey.isEmpty()) {
             return null;
         }
